879b02bdf20dbcb8c7276fc334b924d984883d18
busti <mbust@live.de>
2022-01-06 17:27:23 +0100

    rename window to SeStock

--- src/main/java/de/unihannover/sestock/SeStockApplication.java
+++ src/main/java/de/unihannover/sestock/SeStockApplication.java
@@ -12,7 +12,7 @@ public class SeStockApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SeStockApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("SeStock");
         stage.setScene(scene);
         stage.show();
     }
