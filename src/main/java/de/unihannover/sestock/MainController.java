b855b307cb3e76a85f0097b5a0bb76cb997402b1
beueseba <basti.beuerlein@web.de>
2022-01-06 20:06:06 +0100

    create plot scene

--- src/main/java/de/unihannover/sestock/MainController.java
+++ src/main/java/de/unihannover/sestock/MainController.java
@@ -1,7 +1,12 @@
 package de.unihannover.sestock;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
@@ -10,5 +15,18 @@ public class MainController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("dynamic-plot.fxml"));
+
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 600, 400);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setTitle("Dynamic Stock Plot");
+        stage.setScene(scene);
+        stage.show();
     }
 }
