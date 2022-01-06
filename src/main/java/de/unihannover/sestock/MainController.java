7fbbc42a4eefc44e0f6ec48341cfc69b5042f23c
beueseba <basti.beuerlein@web.de>
2022-01-06 22:02:32 +0100

    add data to plot

--- src/main/java/de/unihannover/sestock/MainController.java
+++ src/main/java/de/unihannover/sestock/MainController.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -36,6 +39,9 @@ public class MainController {
     private Label welcomeText;
 
     @FXML
+    LineChart<Number, Number> chart1;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
         FXMLLoader fxmlLoader = new FXMLLoader();
@@ -47,8 +53,23 @@ public class MainController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         Stage stage = new Stage();
         stage.setTitle("Dynamic Stock Plot");
+
+        XYChart.Series<Number, Number> series1 = new XYChart.Series<>();
+        //series1.setName("Series 1");
+        series1.getData().add(new XYChart.Data<>(1, 20));
+        series1.getData().add(new XYChart.Data<>(2, 100));
+        series1.getData().add(new XYChart.Data<>(3, 80));
+        //series1.getData().add(new XYChart.Data<>(4, 180));
+        //series1.getData().add(new XYChart.Data<>(5, 20));
+        //series1.getData().add(new XYChart.Data<>(6, 10));
+        chart1.getData().add(series1);
+
+//        LineChart<Number, Number> lineChart = new LineChart<Number, Number>(chart1.getXAxis(), chart1.getYAxis());
+//        lineChart.getData().add(series1);
+
         stage.setScene(scene);
         stage.show();
     }
