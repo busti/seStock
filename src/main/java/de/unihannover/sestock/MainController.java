283045155e7f5d449959a0f3fce7100ab178a46c
busti <mbust@live.de>
2022-01-06 20:19:49 +0100

    implement basic combobox with search function

--- src/main/java/de/unihannover/sestock/MainController.java
+++ src/main/java/de/unihannover/sestock/MainController.java
@@ -1,9 +1,30 @@
 package de.unihannover.sestock;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.util.List;
+
 public class MainController {
+    @FXML
+    public ComboBox<String> stockName;
+
+    @FXML
+    protected void onStockNameChange() throws IOException, InterruptedException {
+        var text = stockName.getEditor().getText();
+        List<String> data = AlphavantageApi.getInstance().symbol_search(text);
+        updateComboBoxRecommendations(data);
+        stockName.show();
+    }
+
+    private void updateComboBoxRecommendations(List<String> recomendations) {
+        stockName.getItems().clear();
+        stockName.getItems().addAll(recomendations);
+    }
+
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module de.unihannover.sestock {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.net.http;
+    requires com.google.gson;
 
     opens de.unihannover.sestock to javafx.fxml;
     exports de.unihannover.sestock;
