7b9af3384ad3733c2c0a2e824709890fb28e4da7
beueseba <basti.beuerlein@web.de>
2022-01-15 13:50:41 +0100

    add data to plot and integrate PlotController

--- src/main/java/de/unihannover/sestock/MainController.java
+++ src/main/java/de/unihannover/sestock/MainController.java
@@ -3,9 +3,11 @@ package de.unihannover.sestock;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -16,7 +18,13 @@ public class MainController {
     private Label welcomeText;
 
     @FXML
-    LineChart<Number, Number> chart1;
+    private LineChart<?, ?> LineChartStock;
+
+    @FXML
+    private CategoryAxis x;
+
+    @FXML
+    private NumberAxis y;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -26,28 +34,19 @@ public class MainController {
 
         Scene scene = null;
         try {
-            scene = new Scene(fxmlLoader.load(), 600, 400);
-        } catch (IOException e) {
+            scene = new Scene(fxmlLoader.load(), 600, 500);
+        } catch (
+                IOException e) {
             e.printStackTrace();
         }
 
         Stage stage = new Stage();
         stage.setTitle("Dynamic Stock Plot");
-
-        XYChart.Series<Number, Number> series1 = new XYChart.Series<>();
-        //series1.setName("Series 1");
-        series1.getData().add(new XYChart.Data<>(1, 20));
-        series1.getData().add(new XYChart.Data<>(2, 100));
-        series1.getData().add(new XYChart.Data<>(3, 80));
-        //series1.getData().add(new XYChart.Data<>(4, 180));
-        //series1.getData().add(new XYChart.Data<>(5, 20));
-        //series1.getData().add(new XYChart.Data<>(6, 10));
-        chart1.getData().add(series1);
-
-//        LineChart<Number, Number> lineChart = new LineChart<Number, Number>(chart1.getXAxis(), chart1.getYAxis());
-//        lineChart.getData().add(series1);
-
         stage.setScene(scene);
+
         stage.show();
+
+
+
     }
 }
